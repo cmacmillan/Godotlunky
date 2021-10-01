9a02b1827987e5e0bac784190748f4e807617a97
Chase MacMillan <chasemacmillan@hotmail.com>
2021-10-01 00:27:26 -0700

    started adding main menu screen

---
+++ SpelunkyGodot/MainMenuLights.gd
@@ -0,0 +1,23 @@
+extends ColorRect
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var torchPos = get_node("../CenterContainer/Torch1").global_position
+	var torchPos2 = get_node("../CenterContainer/Torch2").global_position
+	var light1 = Vector2(torchPos.x,get_viewport().get_visible_rect().size.y-torchPos.y+50)
+	var light2 = Vector2(torchPos2.x,get_viewport().get_visible_rect().size.y-torchPos2.y+50)
+	var sm = material as ShaderMaterial
+	sm.set_shader_param("LightPos1",light1)
+	sm.set_shader_param("LightPos2",light2)
+	pass
